d2f223f94317d5c2e9f4ad1e1d467ef97b079274
danzelgeb <danzelgeb@gmx.net>
2025-01-28 11:23:13 +0100

    added cooldown

--- src/main/java/dev/danzel/smash/manager/GameManager.java
+++ src/main/java/dev/danzel/smash/manager/GameManager.java
@@ -1,6 +1,10 @@
 package dev.danzel.smash.manager;
 
 import dev.danzel.smash.Smash;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class GameManager {
     private GameState gameState;
@@ -21,6 +25,20 @@ public class GameManager {
         if (gameState == GameState.LOBBY && Smash.getInstance().getPlayerManager().getPlayers().size() >= 2) {
             gameState = GameState.INGAME;
             //todo start cooldown && random tp
+            final int[] cooldown = {10};
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (cooldown[0] == 0) {
+                        //todo start game
+                        cancel();
+                    }
+                    cooldown[0]--;
+                    Bukkit.getOnlinePlayers().forEach(player -> player.showTitle(
+                            Title.title(Component.text("Das Spiel startet in " + cooldown[0]), Component.text(""))
+                    ));
+                }
+            }.run();
         }
     }
 
